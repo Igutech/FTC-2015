cff58d2794254563c37a4cfe6521ae133fe51a50
Tilman G <tilman.owl@gmail.com>
2015-09-04 18:28:17 -0400

    Example Teleop made by tilman

---
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/TilmansTeleop.java
@@ -0,0 +1,23 @@
+package com.qualcomm.ftcrobotcontroller.opmodes;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.Servo;
+
+/**
+ * Created by Kevin on 8/28/2015.
+ */
+public class TilmansTeleop extends OpMode {
+
+    @Override
+    public void init() {
+
+    }
+
+    @Override
+    public void loop() {
+
+        try {
+            Thread.sleep(10);
+        } catch (Exception e) {  }
+    }
+}
