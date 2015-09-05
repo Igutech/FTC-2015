d09a73ae24f76cceb91a54446fee47c127677471
Logan <pet.freak@yahoo.com>
2015-09-04 20:54:56 -0400

    Logan did experimental things in an attempt to learn what the crap he's doing!

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/LoganWhiteboard.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/LoganWhiteboard.java
@@ -1,22 +1,43 @@
 package com.qualcomm.ftcrobotcontroller.opmodes;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
 
 /**
- * Created by Kevin on 8/28/2015.
  * Edited by Logan at some point in time.
  */
-public class LoganWhiteboard extends OpMode {
 
+public class LoganWhiteboard extends OpMode {
+    Servo servo;
+    TestDCMotorDriver motorDriverL;
+    TestServoDriver servoDriverL;
     @Override
+
     public void init() {
         //Elite Code
+        servoDriverL = new TestServoDriver();
+        motorDriverL = new TestDCMotorDriver(TestRobotConstants.motor);
     }
 
     @Override
     public void loop() {
+        double MSpeedL;
+        MSpeedL = .8;
+        motorDriverL.setMotorSpeed(MSpeedL);
+            try {
+                Thread.sleep(3000);
+            } catch (Exception e) {}
+        MSpeedL = 0;
+        motorDriverL.setMotorSpeed(MSpeedL);
+
 
         try {
+            Thread.sleep(5000);
+        } catch (Exception e) {}
+
+        try {
+
             Thread.sleep(10);
         } catch (Exception e) {  }
     }
