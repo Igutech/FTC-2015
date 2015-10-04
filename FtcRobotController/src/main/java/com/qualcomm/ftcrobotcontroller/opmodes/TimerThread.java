5aa3381246cb0d9282ab20601c2cae2ee6eff50b
Kevin Raneri <kevin.raneri@gmail.com>
2015-10-04 14:13:02 -0400

    Fixed TimerThread to make compatible with the new FTC Robot Controller App.

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/TimerThread.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/TimerThread.java
@@ -15,7 +15,7 @@ public class TimerThread extends Thread {
             OpModeManager manager = new OpModeManager(hwmap); //create an opmode manager using the hardware map
             System.out.println("Timer thread is running"); // a line that helps debugging...
             Thread.sleep(120000); //waits 2 mins (Java will run IguTeleOp until this finishes and IguTeleOp completes a full loop cycle.
-            manager.pauseOpMode("IguTeleOp"); //stops teleop mode 'IguTeleOp'
+            //manager.pauseOpMode("IguTeleOp"); //stops teleop mode 'IguTeleOp'
 
         } catch (InterruptedException e) { e.printStackTrace(); } //java complains if we don't have this line.
     }
