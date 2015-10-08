8d3599b83957b4e5419cde841b26a354281e5d5d
Kevin Raneri <kevin.raneri@gmail.com>
2015-10-07 20:17:19 -0400

    Wrote improved robot drive code. Also created a converter that changes a momentary switch into a 2 position switch.

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/IgutechOldBotTeleop.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/IgutechOldBotTeleop.java
@@ -17,38 +17,71 @@ public class IgutechOldBotTeleop extends OpMode {
 
     double rightPow;
     double leftPow;
+
+    boolean buttonstate;
+    boolean modbuttonstate;
+    int loopstate;
+
     @Override
     public void init() {
         leftmotor = hardwareMap.dcMotor.get("leftmotor");
         rightmotor = hardwareMap.dcMotor.get("rightmotor");
+
+        loopstate = 1;
+        modbuttonstate = false;
     }
+
     public void loop() {
+        buttonstate = gamepad1.right_bumper;
+
         JoyThr = -gamepad1.left_stick_y;
 
-        JoyYaw = gamepad1.right_stick_x;
+        JoyYaw = -gamepad1.right_stick_x;
 
-        if(JoyYaw==0)
-        {
+        if (JoyThr == 0) {
+            rightPow = -JoyYaw;
+            leftPow = JoyYaw;
+        } else if (JoyYaw == 0) {
             rightPow = JoyThr;
             leftPow = JoyThr;
-        }
-        if(JoyYaw<0&&JoyYaw>=-1)
-        {
-            leftPow = ((2*JoyYaw)+1)*JoyThr;
+        } else if (JoyYaw < 0 && JoyYaw >= -1) {
+            leftPow = ((2 * JoyYaw) + 1) * JoyThr;
             rightPow = JoyThr;
             //Joyyaw will be -1 through 0
             //if the joyyaw = -0.5,  0
             //if the joyyaw = -0.75, -.5
             //if the joyyaw = -1.0,  -1
-        }
-        if(JoyYaw>0&&JoyYaw<=1) {
-            rightPow = ((-2*JoyYaw)+1)*JoyThr;
+        } else if (JoyYaw > 0 && JoyYaw <= 1) {
+            rightPow = ((-2 * JoyYaw) + 1) * JoyThr;
             leftPow = JoyThr;
         }
+
+        if (loopstate == 1) {
+            if (buttonstate == true) {
+                loopstate = 2;
+                modbuttonstate = true;
+            }
+        } else if (loopstate == 2) {
+            if (buttonstate == false) {
+                loopstate = 3;
+            }
+        } else if (loopstate == 3) {
+            if (buttonstate == true) {
+                loopstate = 4;
+                modbuttonstate = false;
+            }
+        } else if (loopstate == 4) {
+            if (buttonstate == false) {
+                loopstate = 1;
+            }
+        }
+
         leftmotor.setChannelMode(DcMotorController.RunMode.RUN_WITHOUT_ENCODERS);
         rightmotor.setChannelMode(DcMotorController.RunMode.RUN_WITHOUT_ENCODERS);
         leftmotor.setPower(-rightPow);
         rightmotor.setPower(leftPow);
+
+        telemetry.addData("loopstate", loopstate);
 
         try {
             Thread.sleep(10);
