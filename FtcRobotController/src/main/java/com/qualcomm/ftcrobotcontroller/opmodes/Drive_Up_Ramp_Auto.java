16d5b38f5c4d64ad75583446596fbdc20e106880
Logan <pet.freak@yahoo.com>
2015-10-16 22:02:26 -0400

    Working on a simple autonomous.

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/Drive_Up_Ramp_Auto.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/Drive_Up_Ramp_Auto.java
@@ -3,5 +3,12 @@ package com.qualcomm.ftcrobotcontroller.opmodes;
 /**
  * Created by Logan on 10/16/2015.
  */
-public class Drive_Up_Ramp_Auto {
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
+
+public class Drive_Up_Ramp_Auto extends OpMode {
+
+
 }
