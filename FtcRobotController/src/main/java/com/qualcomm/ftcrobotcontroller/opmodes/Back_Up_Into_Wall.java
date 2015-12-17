890e53fa4401df0f2c2cc6d8dadd0bc248407f91
Logan <pet.freak@yahoo.com>
2015-12-16 20:34:52 -0500

    Fixed the ultrasonic sensor test program.

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/Back_Up_Into_Wall.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/Back_Up_Into_Wall.java
@@ -9,22 +9,25 @@ import com.qualcomm.robotcore.hardware.UltrasonicSensor;
 /**
  * Created by Logan on 12/16/2015.
  */
-public class Back_Up_Into_Wall extends LinearOpMode{
+public class Back_Up_Into_Wall extends LinearOpMode {
     DcMotor Left, Right;
     UltrasonicSensor Ultra1, Ultra2;
     double U1, U2;
+
     @Override
-    public void runOpMode() throws InterruptedException{
+    public void runOpMode() throws InterruptedException {
+
         Left = hardwareMap.dcMotor.get("left2");
         Right = hardwareMap.dcMotor.get("right2");
         Ultra1 = hardwareMap.ultrasonicSensor.get("Ultra1");
         Ultra2 = hardwareMap.ultrasonicSensor.get("Ultra2");
+        while (true) {
+            U1 = Ultra1.getUltrasonicLevel();
+            U2 = Ultra2.getUltrasonicLevel();
 
-        U1 = Ultra1.getUltrasonicLevel();
-        U2 = Ultra2.getUltrasonicLevel();
-
-        telemetry.addData("Ultra1", U1);
-        telemetry.addData("Ultra2", U2);
+            telemetry.addData("Ultra1", U1);
+            telemetry.addData("Ultra2", U2);
+        }
     }
 
 }
